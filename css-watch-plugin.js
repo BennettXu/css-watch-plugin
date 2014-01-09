87c31e74e823c8346dc40ead904cfa5325250011
kuangwk <kuangweike@gmail.com>
2014-01-10 02:52:05 +0800

    add $.watcher.run, stop, reset; finish bootstrap

--- css-watch-plugin.js
+++ css-watch-plugin.js
@@ -1,17 +1,72 @@
 (function($){
 
-	var methods = {}
+  var methods = {}
+  var watcher = {}
+  var items = []
+  var interval = null
+  var time = 100
 
-	methods.watch = function() {
-		console.log('keke')
-		return 'ji'
-	}
+	function Item ($dom, properties) {
+    this.node = $dom
+    this.watchProps = properties
+  }
 
-	methods.unwatch = function() {
-		console.log(this)
-		return 'unwatch'
-	}
+  Item.prototype.extendProperties = function(properties) {
+    var that = this
+    $.each(properties, function(index, prop) {
+      if (that.watchProps.indexOf(prop) === -1 ) {
+        that.watchProps.push(prop)
+      }
+    })
+  }
 
-	jQuery.fn.extend(methods)
+  // methods
+  methods.watch = function(properties) {
+    $dom = this
+    if (item = checkIfInArray($dom)) {
+      item.extendProperties(properties)
+    } else {
+      newItem = new Item($dom, properties)
+      items.push(newItem)
+    }
+    console.log(items)
+  }
+
+  methods.unwatch = function() {
+    console.log(this)
+    return 'unwatch'
+  }
+
+  // watcher
+  watcher.runs = function() {
+    intervalCheck()
+  }
+  watcher.stop = function() {
+    clearInterval(interval)
+  }
+  watcher.reset = function() {
+    items = []
+  }
+
+  function intervalCheck() {
+    interval = setInterval(checkItemsProps, time)
+  }
+
+  function checkItemsProps() {
+    console.log('checkItemsProps')
+  }
+
+  function checkIfInArray($dom) {
+    var foundItem = null
+    $.each(items, function(index, item){
+      if (item.node[0] === $dom[0]) {
+        foundItem = item
+      }
+    })
+    return foundItem
+  }
+
+  jQuery.fn.extend(methods)
+  jQuery.extend( jQuery, {watcher: watcher})
 
 })(jQuery)
--- demo/demo.js
+++ demo/demo.js
@@ -1,2 +1,7 @@
-console.log($.fn.watch())
-console.log($('div').unwatch())
+$.watcher.runs()
+$('div').watch(['height', 'top'])
+$('div').watch(['height', 'width'])
+setTimeout($.watcher.stop, 1000)
+// console.log($('div').unwatch())
+
+
